74ca38b57ca72ad6219577297b46d06103386f6c
Ukeachusim Loveth Ifunanya <ukeloveth247@gmail.com>
2021-08-30 07:03:45 +0100

    .

---
+++ js/array.js
@@ -0,0 +1,18 @@
+//alert("great is our God");
+//console.log(("great is our God"));
+//let items = [23,50,65,18,20];
+//console.log("last elements = "+ items[items.length-1] + " that is the last element");
+//console.log(items.push[120])
+//items.push(120,150,600,450);// adding to the end 
+//items.unshift(200,300,350);//adding to the start
+//items.pop();// to remove the last element
+//items.shift();//to remove form the first element
+//console.log(items)
+ let goods = ["cargo","miller","water","hummer","slumbger","hogger"];
+//console.log("number of goods = " + goods[goods.length-1]);
+//goods.push("bags","napkin","mango")//adds to the end of an array
+//goods.unshift("beans","rice","vegetable")//add to the begin/start of the array
+//goods.pop()//it removes a value from the end of array
+//goods.shift();
+let goodnes = goods.indexOf("popo");
+console.log(goodnes);
---
+++ js/fooddelivery.js
@@ -0,0 +1,70 @@
+//alert("wahaaaaooo");
+// customer_data= [
+// {customer:"Ben10",orders:[22,30,11,17,15,52,27,12],minimumValue:4},
+// {customer:"Sameer",orders:[5,17,30,33,40,22,26,10,11,45],minimumValue:6},
+// {customer:"Zeeshan",orders:[22,30,11,5,17,30,6,57],minimumValue:4}
+
+// ]
+
+// for(i=0; i<customer_data.length;i++){
+// //     console.log(customer_data[i]);
+// //    if(customer.orders >=20 && minimumValue >=5){
+// //    }
+//  if(customer_data[i].orders>=20 && minimumValue>=5){
+//  console.log(customer_data[i]);
+//  }
+// }
+
+// const customer_data = {
+//     'Ben10': [22,30,11,17,15,52,27,12],
+//     'Sameer': [5,17,30,33,40,22,26,10,11,45],
+//     'Zeeshan': [22,30,11,5,17,30,6,57]
+// }
+
+// for(i=0; i>= customer_data.length; i++){
+//     if(customer_data>=20 && customer_data.length>=5){
+//     console.log(customer_data[i]);
+//     }
+// }
+// console.log(customer_data[i]);
+
+//No1
+let people = ["gray","Mary","Davon","James"];
+// for(let j=0;j<people.length;j++){
+//    // console.log(people[j]);
+// }
+//console.log(people[j]);
+
+//NO 2
+// people.shift();
+// console.log(people);
+//no 3
+// people.pop();
+// console.log(people);
+//no4
+// people.unshift("matt");
+// console.log(people);
+//no5
+// people.push(loveth);
+// console.log(people);
+//no 6
+// for(let j=0;j<people.length;j++){
+//    people.splice(1,1);
+//}
+//    console.log(people);
+//no 7
+// people.slice(0,3,"mary","matt");
+// console.log(people);
+//no8
+
+// let foag =   people.indexOf("Mary");
+// console.log(foag);
+//no 9
+let foag =   people.indexOf("Foo");
+console.log(foag);
+//10
+people.slice()
+
+
+
+
